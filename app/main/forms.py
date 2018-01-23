1346a14e3d1e511391cbf5c7cae7ff35fed712d4
zsoltkir1 <tarjanzsolt2@hotmail.com>
2018-01-23 15:02:32 +0100

    alpha version of the card game opened

--- app/main/forms.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import Form
-from wtforms.fields import StringField, SubmitField
-from wtforms.validators import Required
-
-
-class LoginForm(Form):
-    name = StringField('Név', validators=[Required()])
-    #room = StringField('Room', validators=[Required()])
-    submit = SubmitField('Belépek')
